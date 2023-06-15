c744f5c8b675c73aacf3b41b83ac484f10006955
TSPabhishek <91623079+TSPabhishek@users.noreply.github.com>
2023-06-15 23:42:11 +0530

    Add files via upload

---
+++ primeornot.java/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int n = sc.nextInt();
+        boolean isprime = true;
+
+        for(int i = 2 ; i<n-1 ;i++ ) {
+            if (n % i == 0) {
+                isprime = false;
+            }
+        }
+            if(isprime==true){
+                System.out.println("n is prime");
+            }else{
+                System.out.println("n is not prime");
+            }
+        }
+    }
