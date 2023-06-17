7881986f7df1ad707ef2e63b2124b492f2785c2f
TSPabhishek <91623079+TSPabhishek@users.noreply.github.com>
2023-06-17 16:07:18 +0530

    Add files via upload

---
+++ character.java/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 10;
+        char ch = 'a';
+        for(int line = 1 ; line<=n; line++){
+            for(int chars = 1; chars<=line; chars++ ){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
